094b6664ac20230fc57a660aaa0377ec1ed44f1b
ironPriest <iron.priest.mih@gmail.com>
2024-01-12 15:44:21 +0300

    some data inside memory

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,41 @@
 import express, {Request, Response} from 'express'
+
+// create express app
 const app = express()
+
 const port = 3000
+
+const trainings = [
+    {
+        timeStamp: 1212,
+        sets: 4,
+        repsPerSet: 10
+    },
+    {
+        timeStamp: 1313,
+        sets: 5,
+        repsPerSet: 6
+    }
+]
+
+const goals = [
+    {
+        user: 'fooBar',
+        number: 23,
+        date: 1212
+    },
+    {
+        user: 'barFoo',
+        number: 27,
+        date: 1313
+    }
+]
 
 app.get('/', (req: Request, res: Response) => {
     res.send('hello word')
 })
 
+// start app
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
